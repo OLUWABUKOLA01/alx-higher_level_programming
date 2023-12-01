17292485f4d1f8dd89e4765d78c76de471a9c12b
“Oluwabukola <adedotunbukola01@gmail.com>
2023-11-30 17:28:09 -0800

    program that imports the variable

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    from variable_load_5 import a
+    print(a)
