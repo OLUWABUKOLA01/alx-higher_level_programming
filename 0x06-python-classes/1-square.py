243e5c01c88c0d47ed5f17fad71ecca00fad0d09
“Oluwabukola <adedotunbukola01@gmail.com>
2023-12-19 13:09:12 -0800

    a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """type a square."""
+
+    def __init__(self, size):
+        """Initialize the new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
